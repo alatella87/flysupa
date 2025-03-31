aa407b32d583fbc352cd87797fc6a746ad6060d0
Alessandro <alessandro.latella@gmail.com>
2025-03-31 22:36:55 +0200

    [fix] vercel file, vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 8080,
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: { // Add this section
+    outDir: "dist", // Ensure output directory is 'dist'
+  },
+});
