c69cab53262ebcd8087abc79c9e63cc595c27842
Alessandro <alessandro.latella@gmail.com>
2025-04-01 03:37:17 +0200

    [add] Sentry

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import * as Sentry from "@sentry/react";
+
 import { useEffect } from "react";
 import { useLocation, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
-import { ThemeProvider } from "./components/theme-provider"
+import { ThemeProvider } from "./components/theme-provider";
 
 import AuthRedirect from "./components/AuthRedirect";
 import Navbar from "./components/Navbar";
@@ -17,27 +19,45 @@ import EditLesson from "./pages/EditLesson";
 import ConfirmRegistration from "./pages/Auth/ConfirmRegistration";
 import Dashboard from "./pages/Dashboard";
 
+Sentry.init({
+  dsn: "https://6ff7edb8118e1b43ae0236e8653dbdf2@o4508383495716864.ingest.de.sentry.io/4509075451215952",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 function App() {
   const location = useLocation();
 
-  useEffect(() => { 
+  useEffect(() => {
     const loadFlyonui = async () => {
-      const cachedFlyonui = localStorage.getItem('flyonui');
-      
+      const cachedFlyonui = localStorage.getItem("flyonui");
+
       try {
         const flyonui = await import("flyonui/flyonui");
-        
+
         if (!cachedFlyonui) {
-          localStorage.setItem('flyonui', 'loaded');
+          localStorage.setItem("flyonui", "loaded");
         }
         // Check if HSStaticMethods exists before calling
-        if ((window as any).HSStaticMethods && typeof (window as any).HSStaticMethods.autoInit === 'function') {
+        if (
+          (window as any).HSStaticMethods &&
+          typeof (window as any).HSStaticMethods.autoInit === "function"
+        ) {
           (window as any).HSStaticMethods.autoInit();
         } else {
-          console.warn('HSStaticMethods.autoInit is not available');
+          console.warn("HSStaticMethods.autoInit is not available");
         }
       } catch (err) {
-        console.error('Error loading flyonui:', err);
+        console.error("Error loading flyonui:", err);
       }
     };
     loadFlyonui();
