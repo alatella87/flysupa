196295ac2ec52c7e262263fbc9b29fa9d8e314a8
Alessandro <alessandro.latella@gmail.com>
2025-04-10 08:00:46 +0200

    [pusha] pusha

--- src/services/supabaseClient.tsx
+++ src/services/supabaseClient.tsx
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Using Vite environment variables with VITE_ prefix
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = (await import.meta.env.VITE_SUPABASE_URL) as string;
+const supabaseAnonKey = (await import.meta.env
+  .VITE_SUPABASE_ANON_KEY) as string;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error("Supabase URL or Anon Key is missing in environment variables");
