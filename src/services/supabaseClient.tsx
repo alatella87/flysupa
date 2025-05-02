82bfb41d6abfa40b9102c647f97f969cc510122e
Alessandro <alessandro.latella@gmail.com>
2025-05-02 14:55:18 +0200

    [fix] VITE_SUPABASE_ANON_KEY

--- src/services/supabaseClient.tsx
+++ src/services/supabaseClient.tsx
@@ -3,6 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 // Cloud instance
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
